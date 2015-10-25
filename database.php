4b86199ee0e433abc1ea85706b17e84923eff1d3
David Richied <DavidRichied@gmail.com>
2015-10-25 19:47:16 -0400

    Working version of shirts4mike without database

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+try {
+	$db = new PDO("mysql:host=localhost; dbname=shirts4mike; port=3306", "root");
+	var_dump($db);
+} catch (Exception $e) {
+	echo "Could not connect to the database.";
+	exit;
+}
+
+echo "Woo-hoo!";
+
+
+?>
